750200bcf06214a5d7e7da8183ff53e17fb6ed6a
Luke Mattson <lukemattson03@gmail.com>
2025-08-17 12:56:15 -0500

    Add placeholder functionality to add new timers

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+document.body.onload = addElement;
+
 function newTimer() {
     console.log("New timer created");
+    const newDiv = document.createElement("div");
+    const newContent = document.createTextNode("New Timer")
+
+    newDiv.appendChild(newContent);
+    const currentDiv = document.getElementById("timers");
+    document.body.insertBefore(newDiv, currentDiv); 
+
 }
