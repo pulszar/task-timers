7acc7810ec1002b3d30acd129750b93d0eef3373
Luke Mattson <lukemattson03@gmail.com>
2025-08-17 13:50:11 -0500

    Create timer functionality

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-document.body.onload = addElement;
-
 function newTimer() {
-    console.log("New timer created");
+
+    // Create a new timer and append it to the timers section
     const newDiv = document.createElement("div");
     const newContent = document.createTextNode("New Timer")
-
     newDiv.appendChild(newContent);
+
     const currentDiv = document.getElementById("timers");
     document.body.insertBefore(newDiv, currentDiv); 
+    console.log("New timer created");
 
+    var curTime = 0;
+    // Anonymous function so function isn't called immediately
+    setInterval(function() {curTime = timer(curTime);}, 1000);
+
+    
+    function timer(curTime) {
+        console.log(curTime);
+        return curTime += 1;
+    }
 }
